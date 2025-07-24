5e4f086522de008a24e997ca3d8ed4ec4f5d972a
Rushikesh Shelar <rushishelar444@gmail.com>
2025-07-24 19:43:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -6,7 +6,7 @@
   #}
 #}
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "my-bucket" {
     bucket = "test-my-app-bucket-d"
     tags = {
             Name = "test-my-app-bucket-d"
