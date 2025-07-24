1f3975c76ea8f555e7c13f397fd0672bb5dae323
Rushikesh Shelar <rushishelar444@gmail.com>
2025-07-24 19:31:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,29 @@
-terraform {
-  backend "s3" {
-    bucket         = "terra-bucket-3111"
-    key            = "terra/main"
-    region         = "us-east-1"
-    dynamodb_table = "terra-table"
+#terraform {
+  #backend "s3" {
+  #  bucket         = "terra-bucket-3111"
+  #  region         = "us-east-1"
+  #  dynamodb_table = "terra-table"
+  #}
+#}
+
+resource "aws_s3_bucket" "testbucket" {
+    bucket = "test-my-app-bucket-d"
+    tags = {
+            Name = "test-my-app-bucket-d"
+        }
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "tws-junoon-state-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "studentID"
+
+  attribute {
+    name = "studentID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "tws-junoon-state-table"
   }
 }
