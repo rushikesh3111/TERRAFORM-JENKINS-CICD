ae8f17070d00886a782776f78f551a2e946b76c0
Rushikesh Shelar <rushishelar444@gmail.com>
2025-07-24 17:59:08 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-project-bucket-02"
+    bucket         = "backend-012"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-pro-table"
+    dynamodb_table = "terraform-pro-table-2"
   }
 }
